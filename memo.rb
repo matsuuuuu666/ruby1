9ff752b24687164383fc43f6057baf19497f5733
matsu <matsu009xx@gmail.com>
2022-08-29 17:29:38 +0900

    メモアプリ2ndcommit

--- memo.rb
+++ memo.rb
@@ -1,13 +1,13 @@
 require "csv"
 
-puts "1（新規でメモを作成）2（既存のメモを編集する）"
+puts "1（新規でメモを作成）2（既存のメモを編集する）3(既存のメモを削除する)"
 
 memo_type = gets.to_s
 input_number = memo_type.chomp
 
-until input_number === "1" or input_number === "2" do
- puts "1か2の数字を入力してください。"
- puts "1（新規でメモを作成）2（既存のメモを編集する）"
+until input_number === "1" or input_number === "2" or input_number === "3" do
+ puts "1か2か3の数字を入力してください。"
+ puts "1（新規でメモを作成）2（既存のメモを編集する）3(既存のメモを削除する)"
  
  memo_type = gets.to_s
  input_number = memo_type.chomp
@@ -42,5 +42,13 @@ elsif input_number === "2"
   CSV.open("#{file_name}.csv","a") do |csv|
     csv << ["#{memo}"]
   end
-  
+
+elsif input_number === "3"
+  puts "既存のメモを削除します。拡張子を除いたファイル名を入力してください。"
+
+  file_name = gets.chomp
+
+  puts "メモを削除します。"
+
+  File.delete("#{file_name}.csv")
 end
